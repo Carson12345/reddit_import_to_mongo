9053a7f2fa162eaf46903f9bde430176d8a94517
carson ross <carson.yaumy@gmail.com>
2019-03-22 15:50:46 +0800

    update

--- import.py
+++ import.py
@@ -4,12 +4,57 @@ import pandas as pd
 import json
 import time 
 import sys
+import os
+root_dir = "./json_files"
+path = "json_files/"
+
+def updateJson(db, usercoll, fname):
+    filecoll = db['testFile']
+    foundFile = filecoll.find_one({'filename':fname})
+
+    # if (foundFile):
+    #     return
+
+    filecoll.insert_one({'filename':fname})
+    with open(path + fname,'r') as f:
+        for idx, line in enumerate(f):
+            if (idx < 10):
+                row = json.loads(line)
+                found = usercoll.find_one({'author':row['author']})
+                if (found):
+                    real_count = row['count'] - row['not_identified']
+                    old_count = found['count']
+                    old_real_count = found['count'] - found['not_identified']
+                    new_score = (real_count*row['score'] + old_real_count*found['score']) / (old_real_count + real_count)
+                    new_count = old_count + row['count']
+
+                    updateObj = {}
+                    set_obj = {
+                        'count': new_count,
+                        'high': row['high'] + found['high'],
+                        'very_high': row['very_high'] + found['very_high'],
+                        'low': row['low'] + found['low'],
+                        'very Low': row['very Low'] + found['very Low'],
+                        'mixed': row['mixed'] + found['mixed'],
+                        'not_identified': row['not_identified'] + found['not_identified'],
+                        'score': new_score
+                    }
+                    updateObj['$set'] = set_obj
+                    usercoll.find_one_and_update({'_id':found['_id']},updateObj)
+                    print("Found")
+                    print(found)
+                else:
+                    authorObj = row
+                    usercoll.insert_one(authorObj)
+                    print("Insert successful!")
+                    print(row['author'])
+
 
 if __name__ == "__main__":
+    jsonFiles = os.listdir(root_dir) 
+    print( jsonFiles )
     with MongoClient("mongodb://admin:iimt4601@ds019481.mlab.com:19481/iimt4601") as client:
-        db = client.iimt4601
-        usercoll = db['testUsers_1']
-        fname = ''
-        with open(fname,'r') as f:
-            raw = json.loads(f.read())
-            print(raw)
+        for index, fname in enumerate(jsonFiles):
+            db = client.iimt4601
+            usercoll = db['testUsers_1']
+            updateJson(db, usercoll, fname)
